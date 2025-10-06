3472f48b227dad9f5e587a367f5443cffd4465cd
Sidda Deepika <146612535+DeepikaSidda@users.noreply.github.com>
2025-10-06 08:25:45 +0530

    Update application-start.sh

--- aws/scripts/application-start.sh
+++ aws/scripts/application-start.sh
@@ -6,10 +6,10 @@ export JAVA_HOME=/usr/lib/jvm/java-1.8.0-amazon-corretto.x86_64
 export PATH=$JAVA_HOME/bin:$PATH
 
 # Ensure Tomcat ownership
-chown -R tomcat:tomcat /usr/local/tomcat9
+chown -R tomcat:tomcat /opt/tomcat
 
 # Start Tomcat
-/usr/local/tomcat/bin/catalina.sh start
+/opt/tomcat/bin/catalina.sh start
 
 # Optional: wait and check if Tomcat is running
 sleep 10
