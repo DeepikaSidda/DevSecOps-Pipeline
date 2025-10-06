2c95d2282402ad76a92ffb016790c428eaef75b9
Sidda Deepika <146612535+DeepikaSidda@users.noreply.github.com>
2025-10-06 08:05:04 +0530

    Update after-install.sh

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -2,12 +2,12 @@
 set -xe
 
 # Define variables
-S3_BUCKET="s3://my-github-my-codedeploy-artifacts-1"
+S3_BUCKET="s3://my-codedeploy-artifacts-1"
 TOMCAT_WEBAPP="/usr/local/tomcat9/webapps"
 
 # Copy WAR file from S3 bucket to Tomcat webapps folder
-# If you know the exact WAR file name, use it instead of --recursive
-aws s3 cp $S3_BUCKET $TOMCAT_WEBAPP --recursive
+# If you know the exact WAR file name, you can specify it instead of --recursive
+aws s3 cp $S3_BUCKET $TOMCAT_WEBAPP --recursive --region us-east-1
 
 # Ensure the ownership permissions are correct
 chown -R tomcat:tomcat $TOMCAT_WEBAPP
